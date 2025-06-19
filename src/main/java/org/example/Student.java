6eefb3c4f11533de3a444f515ac56a0c54279c6f
Jonnes <jonnes400@gmail.com>
2025-06-19 18:52:59 +0530

    first commit

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Student {
+    private String sname;
+    private int rollNo;
+    private int age;
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
