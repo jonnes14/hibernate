2dd7915d02f5302359f57cd3f00af69dd9178fa1
Jonnes <jonnes400@gmail.com>
2025-07-22 00:07:26 +0530

    worked with Hql queries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,64 +6,34 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import javax.management.*;
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.List;
+import org.hibernate.query.Query;
+
 
 public class Main {
     public static void main(String[] args) {
-        Laptop l=new Laptop();
-        l.setBrand("asus");
-        l.setLid(1);
-        l.setRam(16);
-
-        Laptop l1=new Laptop();
-        l1.setBrand("dell");
-        l1.setLid(2);
-        l1.setRam(17);
-
-        Laptop l2=new Laptop();
-        l2.setBrand("mac");
-        l2.setLid(3);
-        l2.setRam(8);
-
-        Student s=new Student();
-        s.setSname("jonnes");
-        s.setAge(22);
-        s.setRollNo(4);
-
-        Student s1=new Student();
-        s1.setSname("kabilan");
-        s1.setAge(23);
-        s1.setRollNo(5);
 
 
-
-        s.setLaptops(Arrays.asList(l,l1));
-        s1.setLaptops(Arrays.asList(l2));
 
 
         SessionFactory sf=new Configuration()
-                .addAnnotatedClass(org.example.Student.class)
                 .addAnnotatedClass(org.example.Laptop.class)
                 .configure()
                 . buildSessionFactory();
         Session session=sf.openSession();
-        Transaction transaction=session.beginTransaction();
-        session.persist(s);
-        session.persist(s1);
 
-        session.persist(l);
+       Query query=session.createQuery("from Laptop"); // provide all the details
+        List<Laptop> laptops=query.getResultList();
 
-        session.persist(l1);
-        session.persist(l2);
-        transaction.commit();
+
+
   // used for getting      s2=session.get(Student.class,3) ;
-//        session.close();
-//        sf.close();
-        Session session1=sf.openSession();
-        Student s4=session1.get(Student.class,22);
-        session1.close();
-        sf.close();
+    session.close();
+       sf.close();
+
         //System.out.println(l);
     }
 }
